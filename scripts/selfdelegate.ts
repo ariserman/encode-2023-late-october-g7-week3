85d147c6bf514026c139c687ebcdef0ab572abee
seanczx <117126118+seanczx@users.noreply.github.com>
2022-11-21 12:57:12 +0800

    Add files via upload

---
+++ scripts/selfdelegate.ts
@@ -0,0 +1,27 @@
+import { ethers } from "hardhat";
+import { Ballot, Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv"; 
+dotenv.config();
+
+async function main() {
+    const provider = ethers.getDefaultProvider("goerli");
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+    // const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
+    const signer = wallet.connect(provider);
+    console.log(`Connected to the account of address ${signer.address}`);
+  
+
+    const tokenContractFactory = new MyToken__factory(signer);
+    const tokenContract = tokenContractFactory.attach(process.env.TOKEN_CONTRACT_ADDRESS ?? "");
+
+    const delegateTx = await tokenContract.delegate(wallet.address);
+    await delegateTx.wait();
+
+    const votePower = await tokenContract.getVotes(wallet.address);
+    console.log(`Wallet account ${wallet.address} has ${votePower} voting power.`)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
